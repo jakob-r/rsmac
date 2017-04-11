985b2d8d98b48852ebc13b9d7e3a61cb75473aa6
Jakob Richter <code@jakob-r.de>
2017-04-11 11:18:13 +0200

    installSmac

---
+++ R/installSmac.R
@@ -0,0 +1,12 @@
+installSmac = function(dest.path = "~/bin/smac", src = "http://www.cs.ubc.ca/labs/beta/Projects/SMAC/smac-v2.10.03-master-778.tar.gz") {
+  dest.path = "~/bin/smac2"
+  src = "http://www.cs.ubc.ca/labs/beta/Projects/SMAC/smac-v2.10.03-master-778.tar.gz"
+  tdir = tempdir()
+  destfile = file.path(tdir, basename(src))
+  download.file(url = src, destfile = destfile)
+  # tar.file.list = untar(destfile, exdir = tdir, list = TRUE)
+  untar(destfile, exdir = tdir)
+  file.remove(destfile)
+  dir.create(dirname(dest.path), recursive = TRUE, showWarnings = FALSE)
+  system(sprintf("mv %s %s", file.path(tdir, "smac-v2.10.03-master-778"), dest.path))
+}
