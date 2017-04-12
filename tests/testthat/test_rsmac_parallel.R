88d61e340febe1c85486c3c3ae56952cdd1d0e48
Jakob Richter <code@jakob-r.de>
2017-04-12 12:16:48 +0200

    more elaborated parallel test

--- tests/testthat/test_rsmac_parallel.R
+++ tests/testthat/test_rsmac_parallel.R
@@ -12,16 +12,24 @@ test_that("rsmac works in parallel", {
     },
     par.set = makeNumericParamSet(len = 2, lower = -2, upper = 2),
     has.simple.signature = TRUE)
-  scenario = list("use-instances" = "false", runObj = "QUALITY", numberOfRunsLimit = 10)
+  scenario = list("use-instances" = "false", runObj = "QUALITY", numberOfRunsLimit = 5)
   cl.args = list("shared-model-mode" = "true", "shared-model-mode-frequency" = "1")
 
-  parallelMap::parallelStartMulticore(cpus = 2)
+  ncpus = if (identical(Sys.getenv("R_EXPENSIVE_TEST_OK"), "TRUE")) 8 else 2
+
+  parallelMap::parallelStartMulticore(cpus = ncpus)
   parsmac = function(i){
     this.cl.args = c(cl.args, seed = i)
     rsmac(fn.sleep, scenario = scenario, cl.args = this.cl.args, id.smac.run = "parallel-test", cleanup = FALSE, par.id = i)
   }
-  res = parallelMap::parallelMap(parsmac, i = 1:2)
+  res = parallelMap::parallelMap(parsmac, i = 1:ncpus)
   parallelMap::parallelStop()
   expect_class(res[[1]], "OptPath")
-  expect_class(res[[2]], "OptPath")
+  for (i in 2:ncpus) {
+    expect_class(res[[i]], "OptPath")
+    op1 = as.data.frame(res[[i-1]])
+    op2 = as.data.frame(res[[i]])
+    expect_true(nrow(op1), 6)
+    expect_true(all(op1$x1[-1] != op2$x1[-1]))
+  }
 })
